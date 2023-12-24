65afce8a4d2457e612d50f5fa3a972a71c652e5b
Sasorishi <on_alban@yahoo.fr>
2023-12-24 14:47:40 +0100

    Remove amplify, add logo data-issy, grid for charts builder

--- src/components/layout/NavbarLayout.jsx
+++ src/components/layout/NavbarLayout.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 function NavbarLayout() {
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="bg-white border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
+            src="src/assets/imgs/logo-data.png"
+            className="h-14"
+            alt="Data-Issy"
           />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            DevOps
+          <span className="text-black self-center text-2xl font-semibold whitespace-nowrap">
+            Graph Maker
           </span>
         </a>
         <button
@@ -38,7 +38,7 @@ function NavbarLayout() {
             />
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        {/* <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <a
@@ -54,7 +54,7 @@ function NavbarLayout() {
               </button>
             </li>
           </ul>
-        </div>
+        </div> */}
       </div>
     </nav>
   );
