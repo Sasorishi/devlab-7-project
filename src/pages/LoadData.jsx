c872b30b40e5c1be049d37d23512b166896a55f5
Sasorishi <on_alban@yahoo.fr>
2023-12-31 14:20:13 +0100

    Fix body overflow

--- src/pages/LoadData.jsx
+++ src/pages/LoadData.jsx
@@ -45,6 +45,8 @@ function LoadData() {
     setNumberDataset(numberDataset);
   };
 
+  document.body.classList.remove("overflow-hidden");
+
   return (
     <div className="container mx-auto">
       <div className="flex flex-col items-center justify-center h-screen">
