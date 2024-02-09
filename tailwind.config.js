b87e2dcb73a3830534e1ee435a609011f079b0cd
Sasorishi <on_alban@yahoo.fr>
2024-02-09 10:43:28 +0100

    Modal data JSON

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       backgroundColor: {
@@ -15,5 +19,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
