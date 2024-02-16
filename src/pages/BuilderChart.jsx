77a5ccf7f20067e1f964261ec55c7c4f6a8b27a7
Sasorishi <on_alban@yahoo.fr>
2024-02-16 11:24:04 +0100

    Fix json

--- src/pages/BuilderChart.jsx
+++ src/pages/BuilderChart.jsx
@@ -87,7 +87,7 @@ function BuilderChart() {
                     rows="14"
                     className={`block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
                     placeholder="Écrire le JSON..."
-                    value={jsonData}
+                    value={JSON.stringify(Dataset, null, 2)}
                   ></textarea>
                 </div>
               </div>
